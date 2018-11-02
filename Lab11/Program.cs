94dcd37933ee4c674be5e6643b79c93702db29f7
Kristy Currier <kristy.currier@gmail.com>
2018-11-01 20:28:36 -0400

    Movie list complete with function to pull to program

--- Lab11/Program.cs
+++ Lab11/Program.cs
@@ -10,6 +10,42 @@ namespace Lab11
     {
         static void Main(string[] args)
         {
+            List<Movie> listOfMovies = DefaultMovieList(); 
+            
+
         }
+
+        public static List<Movie> DefaultMovieList()
+        {
+            List<Movie> movieList = new List<Movie>();
+
+            movieList.Add(new Movie("Star Wars: A New Hope", "SciFi"));
+            movieList.Add(new Movie("Harry Potter: Deathly Hallows Part 2", "Fantasy"));
+            movieList.Add(new Movie("Moana", "Kids"));
+            movieList.Add(new Movie("Finding Nemo", "Kids"));
+            movieList.Add(new Movie("Planet of the Aps", "SciFi"));
+            movieList.Add(new Movie("Dark Crystal", "Fantasy"));
+            movieList.Add(new Movie("The Princess Bride", "Fantasy"));
+            movieList.Add(new Movie("Incrdibles", "Kids"));
+            movieList.Add(new Movie("The Lion King", "Kids"));
+            movieList.Add(new Movie("Back To the Future", "SciFi"));
+
+            return movieList;
+        }
+    }
+
+
+
+    public class Movie
+    {
+        private string Title { get; set; }
+        private string Category { get; set; }
+
+        public Movie (string title, string category )
+        {
+            Title = title;
+            Category = category;
+        }
+
     }
 }
